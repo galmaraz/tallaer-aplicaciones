6d731a8d73ac9194454ec28c308da2df405c0eeb
yhoselin valero Huanca <yhoselin.valero@uab.edu.bo>
2026-04-23 07:25:47 -0400

    Primer commit note

--- backend/src/note/model/note.model.ts
+++ backend/src/note/model/note.model.ts
@@ -1,24 +1,30 @@
-import { IsBoolean } from "class-validator";
-import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { IsBoolean } from 'class-validator';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 @Entity()
 export class Note {
-    @PrimaryGeneratedColumn({name: 'note_id'})
-    id: number;
+  @PrimaryGeneratedColumn({ name: 'note_id' })
+  id: number;
 
-    @Column()
-    title: string;
+  @Column()
+  title: string;
 
-    @Column()
-    content: string;
+  @Column()
+  content: string;
 
-    @Column()
-    @IsBoolean()
-    activo: boolean;
+  @Column()
+  @IsBoolean()
+  activo: boolean;
 
-    @CreateDateColumn()
-    created_at: Date;
+  @CreateDateColumn()
+  created_at: Date;
 
-    @UpdateDateColumn()
-    updated_at: Date;
+  @UpdateDateColumn()
+  updated_at: Date;
 }
